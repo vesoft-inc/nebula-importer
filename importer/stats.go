9decc2eb3eddaba799445699d4a3e453c9f3d0b5
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-10-17 17:20:31 +0800

    Print real request time

--- importer/stats.go
+++ importer/stats.go
@@ -13,8 +13,8 @@ func InitStatsWorker(ch <-chan Stats) {
 		for {
 			select {
 			case <-ticker.C:
-				log.Printf("nebula requests: finished(%d), latency AVG(%dus), QPS(%.2f/s)",
-					totalCount, totalLatency/totalCount, float64(totalCount)/totalReqTime)
+				log.Printf("nebula requests: finished(%d), latency AVG(%dus), req AVG(%.2fus), QPS(%.2f/s)",
+					totalCount, totalLatency/totalCount, totalReqTime*1000*1000/float64(totalCount), float64(totalCount)/totalReqTime)
 			case stat := <-ch:
 				totalCount++
 				totalReqTime += stat.ReqTime
