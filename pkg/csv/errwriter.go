a4f08b65293e695edd5bd8155a76bfe1c8404fcb
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-11-08 09:07:41 +0800

    Refactor error handler interface

--- pkg/csv/errwriter.go
+++ pkg/csv/errwriter.go
@@ -1,81 +1,38 @@
 package csv
 
 import (
-	"bufio"
 	"encoding/csv"
 	"log"
 	"os"
-	"path"
 
 	"github.com/vesoft-inc/nebula-importer/pkg/base"
-	"github.com/vesoft-inc/nebula-importer/pkg/config"
-	"github.com/vesoft-inc/nebula-importer/pkg/stats"
 )
 
-type CSVErrWriter struct {
-	ErrCh  <-chan base.ErrData
-	FailCh chan<- stats.Stats
-	file   config.File
+type ErrWriter struct {
+	writer    *csv.Writer
+	withLabel bool
 }
 
-func mustCreateFile(filePath string) *os.File {
-	if err := os.MkdirAll(path.Dir(filePath), 0775); err != nil && !os.IsExist(err) {
-		log.Fatal(err)
+func NewErrDataWriter(withLabel bool) *ErrWriter {
+	return &ErrWriter{
+		withLabel: withLabel,
 	}
-	file, err := os.Create(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return file
 }
 
-func (w *CSVErrWriter) InitFile(file config.File, concurrency int) {
-	w.file = file
-	go func() {
-		dataFile := mustCreateFile(file.Error.FailDataPath)
-		defer dataFile.Close()
-
-		dataWriter := csv.NewWriter(dataFile)
-
-		logFile := mustCreateFile(file.Error.LogPath)
-		defer logFile.Close()
-
-		logWriter := bufio.NewWriter(logFile)
-
-		for {
-			rawErr := <-w.ErrCh
-			if rawErr.Error == nil {
-				concurrency--
-				if concurrency == 0 {
-					break
-				} else {
-					continue
-				}
-			}
-
-			w.writeFailedData(dataWriter, rawErr.Data)
-			w.logErrorMessage(logWriter, rawErr.Error)
-
-			w.FailCh <- stats.NewFailureStats(len(rawErr.Data))
-		}
-
-		logWriter.Flush()
-
-		dataWriter.Flush()
-		if err := dataWriter.Error(); err != nil {
-			log.Fatal(err)
-		}
-
-		w.FailCh <- stats.NewFileDoneStats()
-	}()
+func (w *ErrWriter) Error() error {
+	return w.writer.Error()
 }
 
-func (w *CSVErrWriter) writeFailedData(writer *csv.Writer, data []base.Data) {
+func (w *ErrWriter) Init(f *os.File) {
+	w.writer = csv.NewWriter(f)
+}
+
+func (w *ErrWriter) Write(data []base.Data) {
 	if len(data) == 0 {
 		log.Println("Empty error data")
 	}
 	for _, d := range data {
-		if w.file.CSV.WithLabel {
+		if w.withLabel {
 			var record []string
 			switch d.Type {
 			case base.INSERT:
@@ -86,14 +43,13 @@ func (w *CSVErrWriter) writeFailedData(writer *csv.Writer, data []base.Data) {
 				log.Fatalf("Error data type: %s", d.Type)
 			}
 			record = append(record, d.Record...)
-			writer.Write(record)
+			w.writer.Write(record)
 		} else {
-			writer.Write(d.Record)
+			w.writer.Write(d.Record)
 		}
 	}
 }
 
-func (w *CSVErrWriter) logErrorMessage(writer *bufio.Writer, err error) {
-	writer.WriteString(err.Error())
-	writer.WriteString("\n")
+func (w *ErrWriter) Flush() {
+	w.writer.Flush()
 }
