8e7a856a93128ef00a63de97740651dc981e87e6
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-11-26 14:18:41 +0800

    Fix config test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 )
@@ -12,24 +11,15 @@ func TestYAMLParser(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	for i, file := range yaml.Files {
+	for _, file := range yaml.Files {
 		if strings.ToLower(file.Type) != "csv" {
 			t.Fatal("Error file type")
 		}
 		switch strings.ToLower(file.Schema.Type) {
 		case "edge":
-			if i != 0 {
-				t.Fatal("First file is not edge data")
-			}
 		case "vertex":
-			if i != 1 {
-				t.Fatal("Second file is not vertex data")
-			}
-			for j, tag := range file.Schema.Vertex.Tags {
-				tagName := fmt.Sprintf("tag%d", j+1)
-				if tag.Name != tagName {
-					t.Fatalf("Wrong tag name: %s vs. %s", tag.Name, tagName)
-				}
+			if len(file.Schema.Vertex.Tags) == 0 && !file.CSV.WithHeader {
+				t.Fatal("Empty tags in vertex")
 			}
 		default:
 			t.Fatal("Error schema type")
