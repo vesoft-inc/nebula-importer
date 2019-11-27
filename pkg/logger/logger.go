8857eebfceea77a9074c0e321c4b1b2fd21c6610
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-11-27 17:39:37 +0800

    Get caller info

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/vesoft-inc/nebula-importer/pkg/base"
 )
@@ -14,37 +16,85 @@ var logger *log.Logger
 func Init(path string) {
 	file := base.MustCreateFile(path)
 	w := io.MultiWriter(os.Stdout, file)
-	logger = log.New(w, "", log.LstdFlags|log.Lshortfile)
+	logger = log.New(w, "", log.LstdFlags)
 }
 
 func Info(v ...interface{}) {
-	logger.Printf("[INFO] %s", fmt.Sprint(v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Printf("[INFO] %s:%d: %s", file, no, fmt.Sprint(v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Info")
+	}
 }
 
 func Infof(format string, v ...interface{}) {
-	logger.Printf("[INFO] %s", fmt.Sprintf(format, v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Printf("[INFO] %s:%d: %s", file, no, fmt.Sprintf(format, v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Infof")
+	}
 }
 
 func Warn(v ...interface{}) {
-	logger.Printf("[WARN] %s", fmt.Sprint(v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Printf("[WARN] %s:%d: %s", file, no, fmt.Sprint(v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Warn")
+	}
 }
 
 func Warnf(format string, v ...interface{}) {
-	logger.Printf("[WARN] %s", fmt.Sprintf(format, v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Printf("[WARN] %s:%d: %s", file, no, fmt.Sprintf(format, v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Warnf")
+	}
 }
 
 func Error(v ...interface{}) {
-	logger.Printf("[ERROR] %s", fmt.Sprint(v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Printf("[ERROR] %s:%d: %s", file, no, fmt.Sprint(v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Error")
+	}
 }
 
 func Errorf(format string, v ...interface{}) {
-	logger.Printf("[ERROR] %s", fmt.Sprintf(format, v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Printf("[ERROR] %s:%d: %s", file, no, fmt.Sprintf(format, v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Errorf")
+	}
 }
 
 func Fatal(v ...interface{}) {
-	logger.Fatal(fmt.Sprint(v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Fatalf("[FATAL] %s:%d: %s", file, no, fmt.Sprint(v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Fatal")
+	}
 }
 
 func Fatalf(format string, v ...interface{}) {
-	logger.Fatal(fmt.Errorf(format, v...))
+	_, file, no, ok := runtime.Caller(1)
+	if ok {
+		file = filepath.Base(file)
+		logger.Fatalf("[FATAL] %s:%d: %s", file, no, fmt.Sprintf(format, v...))
+	} else {
+		logger.Fatalf("Fail to get caller info of logger.Fatalf")
+	}
 }
