d5b4e2670a45ee6b228347a0a65bf5c766ae0e8d
yixinglu <2520865+yixinglu@users.noreply.github.com>
2019-12-13 16:02:26 +0800

    Fix configure test and test json in yaml

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -14,12 +14,15 @@ func TestYAMLParser(t *testing.T) {
 	}
 
 	for _, file := range yaml.Files {
-		if strings.ToLower(file.Type) != "csv" {
+		if strings.ToLower(*file.Type) != "csv" {
 			t.Fatal("Error file type")
 		}
 		switch strings.ToLower(*file.Schema.Type) {
 		case "edge":
 		case "vertex":
+			if file.Schema.Vertex == nil {
+				continue
+			}
 			if len(file.Schema.Vertex.Tags) == 0 && !*file.CSV.WithHeader {
 				t.Fatal("Empty tags in vertex")
 			}
@@ -48,4 +51,37 @@ func TestTypePointer(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("yaml: %v, %v", *ty.Version, *ty.Files)
+}
+
+var jsonStr = `
+{
+  "version": "beta",
+  "files": [
+    { "path": "/tmp" },
+    { "path": "/etc" }
+	]
+}
+`
+
+func TestJsonInYAML(t *testing.T) {
+	conf := YAMLConfig{}
+	if err := yaml.Unmarshal([]byte(jsonStr), &conf); err != nil {
+		t.Fatal(err)
+	}
+
+	if conf.Version == nil || *conf.Version != "beta" {
+		t.Fatal("Error version")
+	}
+
+	if conf.Files == nil || len(conf.Files) != 2 {
+		t.Fatal("Error files")
+	}
+
+	paths := []string{"/tmp", "/etc"}
+	for i, p := range paths {
+		f := conf.Files[i]
+		if f == nil || f.Path == nil || *f.Path != p {
+			t.Fatalf("Error file %d path", i)
+		}
+	}
 }
