7e3afd8cd524a8183bf9c5691aa76b7dc4b2f382
flyingcat <1004815462@qq.com>
2020-08-07 09:44:29 +0800

    Update importer.go (#84)

--- cmd/importer.go
+++ cmd/importer.go
@@ -20,7 +20,7 @@ func main() {
 	errCode := 0
 	defer func() {
 		// Just for filebeat log fetcher to differentiate following logs from others
-		time.Sleep(10)
+		time.Sleep(10*time.Second)
 		log.Println("--- END OF NEBULA IMPORTER ---")
 		os.Exit(errCode)
 	}()
