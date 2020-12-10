30cb25584b55c686c905ec6c2fe4205ed00aacd9
Yee <2520865+yixinglu@users.noreply.github.com>
2020-12-10 17:10:23 +0800

    Merge 2.0 branch to master  (#102)
    * nebula-importer supports nebula-graph v2-alpha-preview (#98)
    
    * feat: use nebula-go/v2 as nebula-graph client mod.
    
    * feat: wrap the VID value in double quotes when in the INSERT VERTEX statement.
    
    * feat: update metad, storaged, graphd to v2 version in docker-compose.yaml.
    
    * feat: quotes vid value.
    
    * feat: use vesoft/nebula-console:v2-preview-nightly as importer docker image.
    
    * feat: update ci/bootstrap.sh because nebula-console:v2-preview-nightly is based on alpine.
    
    * feat: remove uuid function, and use "(string)hash" instead of hash as vid value.
    
    * feat: use ubuntu-18.04 as github ci server.
    
    * feat: increase afterPeriod wait seconds in example.yaml.
    
    * feat: logger the importer init proceduces.
    
    * feat: add beforePeriod to wait postStart commands competition.
    
    * feat: use ubuntu-latest as github ci server.
    
    * feat: revert to previous afterPeriod.
    
    * feat: remove afterPeriod logger info.
    
    * style: just for re-trigger upstream PR CI.
    
    * Remove vendors (#101)
    
    * Remove vendor
    
    * Add vendorbuild target
    
    * Ignore vendor
    
    * Continue cleanup
    
    * Support nebula 2.0 data import (#99)
    
    * Use nebula go client 2.0
    
    * Try to fix importer tests
    
    * Fix vid type error
    
    * Add more tests
    
    * Rename runner file
    
    * Fix crash
    
    * Fix Ci
    
    * Cleanup
    
    * Cleanup vendors
    
    * Restore 1.0 test csv data
    
    * Fix logger
    
    Co-authored-by: laishzh <laishengzhang@gmail.com>

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -5,19 +5,21 @@ set -e
 addr=$1
 port=$2
 
-curl -fsSL https://studygolang.com/dl/golang/go1.13.4.linux-amd64.tar.gz -o go1.13.4.linux-amd64.tar.gz
-tar zxf go1.13.4.linux-amd64.tar.gz -C /usr/local/
-
-export GOROOT=/usr/local/go
 export GOPATH=/usr/local/nebula/
-export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 export GO111MODULE=on
 
-pushd ./importer/cmd
-go build -mod vendor -o ../../nebula-importer
-popd
+# build nebula-console
+wget "https://github.com/vesoft-inc/nebula-console/archive/master.zip" -O nebula-console.zip
+unzip nebula-console.zip -d .
+mv nebula-console-* nebula-console
+cd nebula-console
+go build -o nebula-console
 
-until echo "quit" | ./bin/nebula -u user -p password --addr=$addr --port=$port &> /dev/null; do
+cd /usr/local/nebula/importer/cmd
+go build -o ../../nebula-importer
+cd /usr/local/nebula
+
+until echo "quit" | /usr/local/nebula/nebula-console/nebula-console -u user -p password --addr=$addr --port=$port &> /dev/null; do
   echo "nebula graph is unavailable - sleeping"
   sleep 2
 done
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/vesoft-inc/nebula-importer
 
 require (
-	github.com/vesoft-inc/nebula-go v1.1.0
+	github.com/vesoft-inc/nebula-clients/go v0.0.0-20201124065101-e86652852b4c
 	gopkg.in/yaml.v2 v2.2.4
 )
 
