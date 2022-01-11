f5c2af2e75af4fd51c2906084f0b3ae8ab7a37ba
Yichen Wang <18348405+Aiee@users.noreply.github.com>
2022-01-11 16:43:38 +0800

    Update nebula go and action (#188)
    * Update nebula go and action
    
    * Fix timestamp format

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -29,5 +29,16 @@ until echo "quit" | /usr/local/nebula/nebula-console -u root -p password --addr=
 done
 
 echo "nebula graph is up - executing command"
+for i in `seq 1 30`;do
+  echo "Adding hosts..."
+  var=`/usr/local/nebula/nebula-console -addr graphd1 -port 9669 -u root -p nebula -e 'ADD HOSTS "storaged":9779'`;
+  if [[ $$? == 0 ]];then
+    echo "Add hosts succeed"
+    break;
+  fi;
+  sleep 2;
+  echo "retry to add hosts.";
+done
+
 ./nebula-importer --config ./importer/examples/v1/example.yaml
 ./nebula-importer --config ./importer/examples/v2/example.yaml
