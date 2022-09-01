37fff33006640ab3cd987c6aecc99289e447b98f
HarrisChu <1726587+HarrisChu@users.noreply.github.com>
2022-09-01 11:14:54 +0800

    add version

--- cmd/importer.go
+++ cmd/importer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -10,15 +11,26 @@ import (
 	"github.com/vesoft-inc/nebula-importer/pkg/config"
 	"github.com/vesoft-inc/nebula-importer/pkg/errors"
 	"github.com/vesoft-inc/nebula-importer/pkg/logger"
+	"github.com/vesoft-inc/nebula-importer/pkg/version"
 	"github.com/vesoft-inc/nebula-importer/pkg/web"
 )
 
 var configuration = flag.String("config", "", "Specify importer configure file path")
+var echoVersion = flag.Bool("version", false, "echo build version")
 var port = flag.Int("port", -1, "HTTP server port")
 var callback = flag.String("callback", "", "HTTP server callback address")
 
 func main() {
 	errCode := 0
+
+	flag.Parse()
+	runnerLogger := logger.NewRunnerLogger("")
+	if *echoVersion {
+		fmt.Printf("%s \n", version.GoVersion)
+		fmt.Printf("Git Hash: %s \n", version.GitHash)
+		fmt.Printf("Tag: %s \n", version.Tag)
+		return
+	}
 	defer func() {
 		// Just for filebeat log fetcher to differentiate following logs from others
 		time.Sleep(1 * time.Second)
@@ -27,11 +39,7 @@ func main() {
 	}()
 
 	log.Println("--- START OF NEBULA IMPORTER ---")
-
-	flag.Parse()
-	runnerLogger := logger.NewRunnerLogger("")
 	if port != nil && *port > 0 && callback != nil && *callback != "" {
-
 		// Start http server
 		svr := &web.WebServer{
 			Port:         *port,
@@ -43,8 +51,8 @@ func main() {
 			panic(err)
 		}
 	} else {
-		if configuration == nil {
-			panic("please configure yaml file")
+		if *configuration == "" {
+			log.Fatal("please configure yaml file")
 		}
 
 		conf, err := config.Parse(*configuration, runnerLogger)
