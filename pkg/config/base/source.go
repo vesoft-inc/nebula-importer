f0347bad9fece17aaa70b4a518001a4336ab4031
Vee Zhang <veezhang@126.com>
2023-09-01 15:29:53 +0800

    feat: hdfs glob (#284)

--- pkg/config/base/source.go
+++ pkg/config/base/source.go
@@ -1,6 +1,9 @@
 package configbase
 
 import (
+	"io/fs"
+	"os"
+
 	"github.com/vesoft-inc/nebula-importer/v4/pkg/reader"
 	"github.com/vesoft-inc/nebula-importer/v4/pkg/source"
 )
@@ -32,4 +35,38 @@ func (s *Source) BuildSourceAndReader(opts ...reader.Option) (
 	rr := reader.NewRecordReader(src)
 	brr := reader.NewBatchRecordReader(rr, opts...)
 	return src, brr, nil
+}
+
+func (s *Source) Glob() ([]*Source, bool, error) {
+	sourceConfig := s.SourceConfig
+	src, err := sourceNew(&sourceConfig)
+	if err != nil {
+		return nil, false, err
+	}
+
+	g, ok := src.(source.Globber)
+	if !ok {
+		// Do not support glob.
+		return nil, false, nil
+	}
+	defer src.Close()
+
+	cs, err := g.Glob()
+	if err != nil {
+		return nil, true, err
+	}
+
+	if len(cs) == 0 {
+		return nil, true, &os.PathError{Op: "open", Path: src.Name(), Err: fs.ErrNotExist}
+	}
+
+	ss := make([]*Source, 0, len(cs))
+	for _, c := range cs {
+		cpy := *s
+		cpySourceConfig := c.Clone()
+		cpy.SourceConfig = *cpySourceConfig
+		ss = append(ss, &cpy)
+	}
+
+	return ss, true, nil
 }
--- pkg/config/base/source_test.go
+++ pkg/config/base/source_test.go
@@ -72,4 +72,153 @@ var _ = Describe("Source", func() {
 			Expect(brr).To(BeNil())
 		})
 	})
+
+	Describe(".Glob", func() {
+		var (
+			s           *Source
+			ctrl        *gomock.Controller
+			mockSource  *source.MockSource
+			mockGlobber *source.MockGlobber
+			patches     *gomonkey.Patches
+		)
+		BeforeEach(func() {
+			ctrl = gomock.NewController(GinkgoT())
+			mockSource = source.NewMockSource(ctrl)
+			mockGlobber = source.NewMockGlobber(ctrl)
+			patches = gomonkey.NewPatches()
+			s = &Source{
+				SourceConfig: source.Config{
+					Local: &source.LocalConfig{
+						Path: "path*",
+					},
+					CSV: &source.CSVConfig{
+						Delimiter: ",",
+					},
+				},
+				Batch: 7,
+			}
+		})
+		AfterEach(func() {
+			ctrl.Finish()
+			patches.Reset()
+		})
+
+		It("failed", func() {
+			patches.ApplyGlobalVar(&sourceNew, func(_ *source.Config) (source.Source, error) {
+				return nil, stderrors.New("test error")
+			})
+			ss, isSupportGlob, err := s.Glob()
+			Expect(err).To(HaveOccurred())
+			Expect(isSupportGlob).To(Equal(false))
+			Expect(ss).To(BeNil())
+		})
+
+		It("unsupported", func() {
+			patches.ApplyGlobalVar(&sourceNew, func(_ *source.Config) (source.Source, error) {
+				return mockSource, nil
+			})
+			ss, isSupportGlob, err := s.Glob()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(isSupportGlob).To(Equal(false))
+			Expect(ss).To(BeNil())
+		})
+
+		It("failed at glob", func() {
+			patches.ApplyGlobalVar(&sourceNew, func(_ *source.Config) (source.Source, error) {
+				return struct {
+					*source.MockSource
+					*source.MockGlobber
+				}{
+					MockSource:  mockSource,
+					MockGlobber: mockGlobber,
+				}, nil
+			})
+			mockGlobber.EXPECT().Glob().Return(nil, stderrors.New("test error"))
+			mockSource.EXPECT().Close().Return(nil)
+
+			ss, isSupportGlob, err := s.Glob()
+			Expect(err).To(HaveOccurred())
+			Expect(isSupportGlob).To(Equal(true))
+			Expect(ss).To(BeNil())
+		})
+
+		It("glob return empty", func() {
+			patches.ApplyGlobalVar(&sourceNew, func(_ *source.Config) (source.Source, error) {
+				return struct {
+					*source.MockSource
+					*source.MockGlobber
+				}{
+					MockSource:  mockSource,
+					MockGlobber: mockGlobber,
+				}, nil
+			})
+			mockGlobber.EXPECT().Glob().Return(nil, nil)
+			mockSource.EXPECT().Name().AnyTimes().Return("source name")
+			mockSource.EXPECT().Close().Return(nil)
+
+			ss, isSupportGlob, err := s.Glob()
+			Expect(err).To(HaveOccurred())
+			Expect(isSupportGlob).To(Equal(true))
+			Expect(ss).To(BeNil())
+		})
+
+		It("glob return many", func() {
+			patches.ApplyGlobalVar(&sourceNew, func(_ *source.Config) (source.Source, error) {
+				return struct {
+					*source.MockSource
+					*source.MockGlobber
+				}{
+					MockSource:  mockSource,
+					MockGlobber: mockGlobber,
+				}, nil
+			})
+			mockGlobber.EXPECT().Glob().Return([]*source.Config{
+				{
+					Local: &source.LocalConfig{
+						Path: "path1",
+					},
+					CSV: &source.CSVConfig{
+						Delimiter: ",",
+					},
+				},
+				{
+					Local: &source.LocalConfig{
+						Path: "path2",
+					},
+					CSV: &source.CSVConfig{
+						Delimiter: ",",
+					},
+				},
+			}, nil)
+			mockSource.EXPECT().Close().Return(nil)
+
+			ss, isSupportGlob, err := s.Glob()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(isSupportGlob).To(Equal(true))
+			Expect(ss).To(Equal([]*Source{
+				{
+					SourceConfig: source.Config{
+						Local: &source.LocalConfig{
+							Path: "path1",
+						},
+						CSV: &source.CSVConfig{
+							Delimiter: ",",
+						},
+					},
+					Batch: 7,
+				},
+				{
+					SourceConfig: source.Config{
+						Local: &source.LocalConfig{
+							Path: "path2",
+						},
+						CSV: &source.CSVConfig{
+							Delimiter: ",",
+						},
+					},
+					Batch: 7,
+				},
+			}))
+		})
+	})
 })
